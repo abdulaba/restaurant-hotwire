0aa077c9be2490e3f18cebab2e36fb6ae30d519c
Andrés Ibáñez Ulloa <andresibanezulloa@gmail.com>
2024-03-10 10:28:01 -0300

    restaurant controller

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -10,18 +10,32 @@ class RestaurantController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = current_user.restaurants.build(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path, notice: "El restaurante se creó correctamente"
+    else
+      render :new, status: :unprocessable_entity, alert: "Hubo un error al crear el restaurante"
+    end
   end
 
   def edit
   end
 
   def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurants_path, notice: "El restaurante se editó correctamente"
+    else
+      render :edit, status: :unprocessable_entity, alert: "Hubo un error al crear el restaurante"
+    end
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
   end
 
   private
